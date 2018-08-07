9db62cd2208dac0d30c67e3d40ba997c3affd278
Stephane Lafontaine <stephane@medqigong.ca>
2018-08-07 12:49:16 -0400

    Added create method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,4 +7,16 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(title: params[:task][:title], details: params[:task][:details])
+    @task.save
+    # raise
+    redirect_to task_path(@task)
+  end
+
 end
