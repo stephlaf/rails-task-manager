6344bc7b8eb01efcb3aebdbba9f89c20f9ef3a00
Stephane Lafontaine <stephane@medqigong.ca>
2018-08-07 14:43:39 -0400

    Done without style

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,13 @@
 class TasksController < ApplicationController
 
+  before_action :fetch_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
   end
 
   def new
@@ -20,15 +22,48 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
-    @task.update(title: params[:task][:title], details: params[:task][:details])
+    # @task = Task.find(params[:id])
+    @task.update(title: params[:task][:title], details: params[:task][:details], completed: params[:task][:completed])
     
     redirect_to task_path(@task)
   end
 
+  def destroy
+    # @task = Task.find(params[:id])
+    @task.destroy
+    
+    redirect_to tasks_path
+  end
+
+  private
+
+  def tasks_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def fetch_task
+    @task = Task.find(params[:id])
+  end
 
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,6 @@ Rails.application.routes.draw do
   get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
   patch 'tasks/:id', to: 'tasks#update'
 
+  delete 'tasks/:id', to: 'tasks#destroy', as: 'destroy_task'
+  
 end
