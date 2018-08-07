d1ae509c27d47a4171ba765947440abda299c39a
Stephane Lafontaine <stephane@medqigong.ca>
2018-08-07 13:55:28 -0400

    Added update method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -19,4 +19,16 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(title: params[:task][:title], details: params[:task][:details])
+    
+    redirect_to task_path(@task)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,7 @@ Rails.application.routes.draw do
   get '/tasks', to: 'tasks#index'
   get '/tasks/:id', to: 'tasks#show', as: 'task'
 
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  patch 'tasks/:id', to: 'tasks#update'
+
 end
